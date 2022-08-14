7e720ca0f2357680e1015409a876e4866649925d
ahtoh <lanetchii.anton@gmail.com>
2022-08-14 17:12:48 +0300

    just_one_alt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <header className='header'>
-          <Link to='/profile'><img src='https://cryptologos.cc/logos/aave-aave-logo.png' /></Link>
+          <Link to='/profile'><img src='https://cryptologos.cc/logos/aave-aave-logo.png' alt='logo_button' /></Link>
         </header>
     )
 }
