3746fa190c8038eebd389fb689e7b8e51ae7bcf1
ahtoh <lanetchii.anton@gmail.com>
2022-11-05 16:59:56 +0200

    users page fixed

--- src/components/find_users/FindUsers.jsx
+++ src/components/find_users/FindUsers.jsx
@@ -1,6 +1,24 @@
 import './findUsers.css';
 
 const FindUsers = (props) => {
+
+    if (props.users.length === 0) {
+        props.setUsers([
+            {
+                id: 1, photourl: 'https://www.arlis.umd.edu/sites/default/files/default_images/avatardefault_92824.png',
+                followed: false, fullname: 'Sasha D.', status: 'fuck it', location: { country: 'Moldova', city: 'Kishinev' }
+            },
+            {
+                id: 2, photourl: 'https://www.arlis.umd.edu/sites/default/files/default_images/avatardefault_92824.png',
+                followed: true, fullname: 'Vanya B.', status: 'fuck you', location: { country: 'Moldova', city: 'Kishinev' }
+            },
+            {
+                id: 3, photourl: 'https://www.arlis.umd.edu/sites/default/files/default_images/avatardefault_92824.png',
+                followed: false, fullname: 'Sasha S.', status: 'i don`t care', location: { country: 'USA', city: 'Texas' }
+            },
+        ])
+    }
+
     return <div>
         {
             props.users.map(u => <div key={u.id}>
@@ -9,8 +27,8 @@ const FindUsers = (props) => {
                         <img src={u.photourl} alt='userPhoto' width={50} />
                     </div>
                     <div>
-                        { u.followed ? <button className='button' onClick={ () => {props.unfollow(u.id)}} >Follow</button>
-                        : <button className='button' onClick={ () => {props.follow(u.id)} } >Unfollow</button> }
+                        {u.followed ? <button className='button' onClick={() => { props.unfollow(u.id) }} >Follow</button>
+                            : <button className='button' onClick={() => { props.follow(u.id) }} >Unfollow</button>}
                     </div>
                 </span>
                 <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -5,14 +5,7 @@ const SET_USERS = 'SET_USERS';
 
 
 let initialState = {
-    users: [
-        { id: 1, photourl: 'https://www.arlis.umd.edu/sites/default/files/default_images/avatardefault_92824.png', 
-        followed: false, fullname: 'Sasha D.', status: 'fuck it', location: { country: 'Moldova', city: 'Kishinev' } },
-        { id: 2, photourl: 'https://www.arlis.umd.edu/sites/default/files/default_images/avatardefault_92824.png', 
-        followed: true, fullname: 'Vanya B.', status: 'fuck you', location: { country: 'Moldova', city: 'Kishinev' } },
-        { id: 3, photourl: 'https://www.arlis.umd.edu/sites/default/files/default_images/avatardefault_92824.png', 
-        followed: false, fullname: 'Sasha S.', status: 'i don`t care', location: { country: 'USA', city: 'Texas' } },
-    ]
+    users: [ ]
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -34,6 +27,7 @@ const usersReducer = (state = initialState, action) => {
                     if (u.id === action.userID) {
                         return { ...u, followed: false }
                     }
+                    return u;
                 } )
             }
 
